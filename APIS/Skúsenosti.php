877a41582c50b3cbfdef1bbc7115c104063dce72
MCCANON <palko.cibula@gmial.com>
2026-05-20 12:21:44 +0200

    20.5.2026 (prax)

---
+++ APIS/Skúsenosti.php
@@ -0,0 +1,14 @@
+<?php
+$cards = [
+    [
+        'icon'  => '',
+        'title' => 'Výrazne sme znížili administratívnu a plánovanie zamestnaneckých kapacít v rámci kuriérnych operácií.',
+        'desc'  => 'Peter Radačovský',
+    ],
+    [
+        'icon'  => 'ti-cup',
+        'title' => 'Stravovanie',
+    ]
+];
+?>
+
